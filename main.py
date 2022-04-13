afb621a630efbbba38d615a8142ffa4dae6f4763
eengamer2007 <eengamer2007@gmail.com>
2022-04-13 08:54:44 +0200

    we have ground and background

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import pygame
 from math import sin,cos,pi
 
+WIN_WIDTH  = 750
+WIN_HEIGHT = 500
+
 def main():
     pygame.init()
+    surface = pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+    running = 1
     while running:
+        surface.fill((255,255,255))
+        pygame.draw.rect(surface, (0,255,0), pygame.Rect(0,WIN_HEIGHT-100,WIN_WIDTH,100))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
